782d72255613052695ae360d51947f42f0a6a914
Darvis <dsouzavishal685@gmail.com>
2026-08-15 17:31:09 +0530

    remove the -v (verbose) flag from tar as that prints every extracted file to stdout

--- build.sh
+++ build.sh
@@ -8,32 +8,25 @@ LLVM_SRC="${WORKSPACE_DIR}/llvm-src"
 
 curl -L "https://github.com/llvm/llvm-project/releases/download/llvmorg-${LLVM_VERSION}/${LLVM_ARCHIVE}" -o "${LLVM_ARCHIVE}"
 
-tar -xvf "${LLVM_ARCHIVE}"
+tar -xf "${LLVM_ARCHIVE}"
 mv "llvm-project-${LLVM_VERSION}.src" "${LLVM_SRC}"
 rm "${LLVM_ARCHIVE}"
 
 HOST_DIR="${WORKSPACE_DIR}/build-host"
 
-cmake -S "${LLVM_SRC}/llvm" \
-  -B "${HOST_DIR}" \
-  -G Ninja \
+cmake -S "${LLVM_SRC}/llvm" -B "${HOST_DIR}"  -G Ninja \
   -DCMAKE_BUILD_TYPE=Release \
   -DLLVM_ENABLE_PROJECTS="clang" \
   -DLLVM_TARGETS_TO_BUILD="AArch64"
 
-cmake --build "${HOST_BUILD}" \
-  --target llvm-tblgen clang-tblgen \
-  --parallel "$(sysctl -n hw.ncpu)"
+cmake --build "${HOST_BUILD}" --target llvm-tblgen clang-tblgen --parallel "$(sysctl -n hw.ncpu)"
 
-IOS_SDK_PATH="$(xcrun --sdk iphoneos --show-sdk-path)"
 IOS_BUILD="${WORKSPACE_DIR}/build-ios"
 
-cmake -S "${LLVM_SRC}/llvm" \
-  -B "${IOS_BUILD}" \
-  -G Ninja \
+cmake -S "${LLVM_SRC}/llvm"  -B "${IOS_BUILD}"  -G Ninja \
   -DCMAKE_BUILD_TYPE=Release \
   -DCMAKE_SYSTEM_NAME=iOS \
-  -DCMAKE_OSX_SYSROOT="${IOS_SDK_PATH}" \
+  -DCMAKE_OSX_SYSROOT="$(xcrun --sdk iphoneos --show-sdk-path)" \
   -DCMAKE_OSX_ARCHITECTURES="arm64" \
   -DCMAKE_OSX_DEPLOYMENT_TARGET="15.0" \
   -DLLVM_ENABLE_PROJECTS="clang" \
@@ -41,5 +34,4 @@ cmake -S "${LLVM_SRC}/llvm" \
   -DLLVM_DEFAULT_TARGET_TRIPLE="arm64-apple-ios15.0" \
   -DLLVM_NATIVE_TOOL_DIR="${HOST_BUILD}/bin"
 
-cmake --build "${IOS_BUILD}" \
-  --parallel "$(sysctl -n hw.ncpu)"
+cmake --build "${IOS_BUILD}" --parallel "$(sysctl -n hw.ncpu)"
