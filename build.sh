ab061c0726363c8cdb09d2455f3214cea37985e7
Darvis <dsouzavishal685@gmail.com>
2026-08-16 16:42:26 +0530

    test with llvm 22.1.8

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-LLVM_VERSION="17.0.6"
+LLVM_VERSION="22.1.8"
 WORKSPACE_DIR=$(pwd)
 LLVM_ARCHIVE="llvm-project-${LLVM_VERSION}.src.tar.xz"
 LLVM_SRC="${WORKSPACE_DIR}/llvm-src"
@@ -15,7 +15,7 @@ rm "${LLVM_ARCHIVE}"
 HOST_BUILD="${WORKSPACE_DIR}/build-host"
 
 cmake -S "${LLVM_SRC}/llvm" -B "${HOST_BUILD}"  -G Ninja \
-  -Wno-dev \
+  -Wno-author \
   -Wno-deprecated \
   -DCMAKE_BUILD_TYPE=Release \
   -DLLVM_ENABLE_PROJECTS="clang" \
@@ -26,18 +26,19 @@ cmake --build "${HOST_BUILD}" --target llvm-tblgen clang-tblgen --parallel "$(sy
 IOS_BUILD="${WORKSPACE_DIR}/build-ios"
 
 cmake -S "${LLVM_SRC}/llvm"  -B "${IOS_BUILD}"  -G Ninja \
-  -Wno-dev \
+  -Wno-author \
   -Wno-deprecated \
   -DCMAKE_BUILD_TYPE=Release \
-  -DCMAKE_C_FLAGS="-target arm64-apple-ios15.0 -w" \
-  -DCMAKE_CXX_FLAGS="-target arm64-apple-ios15.0 -w" \
+  -DCMAKE_SYSTEM_NAME=iOS \
+  -DCMAKE_C_FLAGS="-w" \
+  -DCMAKE_CXX_FLAGS="-w" \
+  -DCMAKE_MACOSX_BUNDLE=OFF \
   -DCMAKE_OSX_SYSROOT="$(xcrun --sdk iphoneos --show-sdk-path)" \
   -DCMAKE_OSX_ARCHITECTURES="arm64" \
   -DCMAKE_OSX_DEPLOYMENT_TARGET="15.0" \
   -DLLVM_ENABLE_PROJECTS="clang" \
   -DLLVM_TARGETS_TO_BUILD="AArch64" \
   -DLLVM_DEFAULT_TARGET_TRIPLE="arm64-apple-ios15.0" \
-  -DLLVM_NATIVE_TOOL_DIR="${HOST_BUILD}/bin" \
-  -DLLVM_ENABLE_ZSTD=OFF
+  -DLLVM_NATIVE_TOOL_DIR="${HOST_BUILD}/bin"
 
 cmake --build "${IOS_BUILD}" --parallel "$(sysctl -n hw.ncpu)"
