7b263caf3a8abd3df8ed51941077bd6d222f71ea
Darvis <dsouzavishal685@gmail.com>
2026-08-17 22:10:14 +0530

    Add host dir for the 3rd method

--- build-cross-cmake.sh
+++ build-cross-cmake.sh
@@ -66,6 +66,18 @@ perl -0pi -e 's/if\(NOT LLVM_NO_DEAD_STRIP\)\n\s+if\("\$\{CMAKE_SYSTEM_NAME\}" M
 HANDLELLVMOPTIONS="${LLVM_SRC}/llvm/cmake/modules/HandleLLVMOptions.cmake"
 perl -0pi -e 's/CMAKE_SYSTEM_NAME MATCHES "Darwin\|FreeBSD/CMAKE_SYSTEM_NAME MATCHES "Darwin|iOS|FreeBSD/' "${HANDLELLVMOPTIONS}"
 
+echo "Building host TableGen utils"
+HOST_BUILD="${WORKSPACE_DIR}/build-host"
+
+cmake -S "${LLVM_SRC}/llvm" -B "${HOST_BUILD}"  -G Ninja \
+  -Wno-author \
+  -Wno-deprecated \
+  -DCMAKE_BUILD_TYPE=Release \
+  -DLLVM_ENABLE_PROJECTS="clang" \
+  -DLLVM_TARGETS_TO_BUILD="AArch64;X86"
+
+cmake --build "${HOST_BUILD}" --target llvm-tblgen clang-tblgen --parallel "$(sysctl -n hw.ncpu)"
+
 echo "Building ${TARGET}"
 BUILD_DIR="${WORKSPACE_DIR}/build-${TARGET}-$(echo "${BUILD_TYPE}" | tr '[:upper:]' '[:lower:]')"
 
@@ -82,6 +94,7 @@ cmake -S "${LLVM_SRC}/llvm" -B "${BUILD_DIR}" -G Ninja \
   -DCMAKE_OSX_DEPLOYMENT_TARGET="15.0" \
   -DLLVM_ENABLE_PROJECTS="clang" \
   -DLLVM_TARGETS_TO_BUILD="AArch64;X86" \
-  -DLLVM_DEFAULT_TARGET_TRIPLE="${TRIPLE}"
+  -DLLVM_DEFAULT_TARGET_TRIPLE="${TRIPLE}" \
+  -DLLVM_NATIVE_TOOL_DIR="${HOST_BUILD}/bin"
 
 cmake --build "${BUILD_DIR}" --parallel "$(sysctl -n hw.ncpu)"
